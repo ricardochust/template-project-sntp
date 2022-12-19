199d4b70f920627c0c43952aa05fdb0f6b2df6e7
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2022-12-19 10:37:59 +0100

    initialize template project

---
+++ Debug/Core/Src/main.d
@@ -0,0 +1,154 @@
+Core/Src/main.o: ../Core/Src/main.cpp ../Core/Inc/main.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h \
+ ../Core/Inc/stm32h7xx_hal_conf.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h723xx.h \
+ ../Drivers/CMSIS/Include/core_cm7.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_ll_adc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_fdcan.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cordic.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_fmac.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_lptim.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rtc.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rtc_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_tim.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_tim_ex.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h \
+ ../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/HALAL.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/DigitalOutputservice/DigitalOutputservice.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/PinModel/Pin.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/C++Utilities/CppUtils.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/DigitalInputService/DigitalInputService.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Flash/Flash.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Flash/FlashTests/Flash_Test.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/ADC/ADC.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/LowPowerTimer/LowPowerTimer.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/PWMservice/PWMservice.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/TimerPeripheral/TimerPeripheral.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Time/Time.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/InputCapture/InputCapture.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Encoder/Encoder.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/EXTI/EXTI.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/SPI/SPI.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/Packets/RawPacket.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/UART/UART.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/UDP/DatagramSocket.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/TCP/ServerSocket.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/TCP/Socket.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/Ethernet.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/ST-LIB_LOW.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Clocks/Counter.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Clocks/Stopwatch.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/PWM/PWM.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/DigitalOutput/DigitalOutput.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/ST-LIB_HIGH.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/Protections/Protection.hpp \
+ C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/Protections/ProtectionManager.hpp
+../Core/Inc/main.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal.h:
+../Core/Inc/stm32h7xx_hal_conf.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h7xx.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/stm32h723xx.h:
+../Drivers/CMSIS/Include/core_cm7.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32H7xx/Include/system_stm32h7xx.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rcc_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_gpio_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_dma_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_ll_adc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_fdcan.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cordic.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_fmac.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_lptim.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rtc.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_rtc_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_tim.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_tim_ex.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h:
+../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/HALAL.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/DigitalOutputservice/DigitalOutputservice.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/PinModel/Pin.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/C++Utilities/CppUtils.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/DigitalInputService/DigitalInputService.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Flash/Flash.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Flash/FlashTests/Flash_Test.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/ADC/ADC.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/LowPowerTimer/LowPowerTimer.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/PWMservice/PWMservice.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/TimerPeripheral/TimerPeripheral.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Time/Time.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/InputCapture/InputCapture.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Encoder/Encoder.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/EXTI/EXTI.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/SPI/SPI.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Models/Packets/RawPacket.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/UART/UART.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/UDP/DatagramSocket.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/TCP/ServerSocket.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/TCP/Socket.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/HALAL/Services/Communication/Ethernet/Ethernet.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/ST-LIB_LOW.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Clocks/Counter.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Clocks/Stopwatch.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/PWM/PWM.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/DigitalOutput/DigitalOutput.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_LOW/Actuator/HalfBridge/HalfBridge.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/ST-LIB_HIGH.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/Protections/Protection.hpp:
+C:/Users/aleja/Proyectos/Hyper-H8/ST-LIB/Inc/ST-LIB_HIGH/Protections/ProtectionManager.hpp:
