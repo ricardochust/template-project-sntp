46a1a2be2430665e42aaf2e882a69b044175a40b
ricardo chust <ricardochustfides@gmail.com>
2023-02-01 13:48:27 +0100

    time to low time

--- Core/Src/Examples/examples_includes.hpp
+++ Core/Src/Examples/examples_includes.hpp
@@ -2,5 +2,5 @@
 #include "input_capture_example.hpp"
 #include "sensors_example.hpp"
 #include "exti_example.hpp"
-#include "time_example.hpp"
 #include "clock_example.hpp"
+#include "low_time_example.hpp"
