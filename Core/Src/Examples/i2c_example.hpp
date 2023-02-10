e37020c41f88814f3c49f9ec79898763214db235
ricardo chust <ricardochustfides@gmail.com>
2023-02-10 22:50:14 +0100

    i2c changed

--- Core/Src/Examples/i2c_example.hpp
+++ Core/Src/Examples/i2c_example.hpp
@@ -13,12 +13,25 @@ int i2c_example(void)
 	uint8_t id = I2C::inscribe(I2C::i2c2).value_or(69);
 	STLIB::start(Nucleo);
 	I2C::start();
-	I2CPacket paquete(0,4);
+	uint8_t buf[4];
+	buf[0] = 1;
+	buf[1] = 2;
+	buf[2] = 4;
+	buf[3] = 8;
+	I2CPacket paquete(4,buf,4);
 	printf("\n\n\n\n\n\n\n\n\n\n");
 
 	while (1) {
 		if(!I2C::is_busy(id)){
-			I2C::transmit_next_packet(id, paquete);
+			if(I2C::transmit_next_packet(id, paquete)){
+				while(!I2C::has_next_packet(id)){
+
+				}
+				if(I2C::receive_next_packet(id, paquete)){
+					printf("done");
+				}
+
+			}
 		}else{
 			printf("i m busy");
 			HAL_Delay(100);
