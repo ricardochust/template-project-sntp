6f13f786e782e2c90e134b024f6299e53f714944
ricardo chust <ricardochustfides@gmail.com>
2023-02-28 17:45:31 +0100

    sntp a

--- Core/Src/Examples/rtc_example.hpp
+++ Core/Src/Examples/rtc_example.hpp
@@ -11,13 +11,15 @@
 
 int rtc_example(void){
 
-	STLIB::start(Nucleo);
+	STLIB::start(Nucleo,"192.168.1.3","255.255.255.0","192.168.1.4");
 
-	//Time::set_RTC_data(0,30,20,10,28,2,2023);
+	SNTP::sntp_update();
+
+	Time::set_RTC_data(0,30,20,10,28,2,2023);
 	printf("\n\n\n\n\n\n\n\n\n\n");
 	while(1){
-		//Time::rtc_data now = Time::get_RTC_data();
-		//printf("%d:%d:%d::%d  %d-%d-%d \n",now.hour,now.minute,now.second,now.counter,now.day,now.month,now.year);
+		Time::rtc_data now = Time::get_RTC_data();
+		printf("%d:%d:%d::%d  %d-%d-%d \n",now.hour,now.minute,now.second,now.counter,now.day,now.month,now.year);
 		HAL_Delay(100);
 	}
 }
